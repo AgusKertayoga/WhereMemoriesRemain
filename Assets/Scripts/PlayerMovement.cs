83922db14d1ab7f6adc815eb9d4fbdd8d82e3ad0
I Kadek Agus Kertayoga <aguskertayoga@gmail.com>
2026-06-30 23:02:04 +0800

    project start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+
+        if (move > 0)
+            sr.flipX = false;
+        else if (move < 0)
+            sr.flipX = true;
+    }
+}
